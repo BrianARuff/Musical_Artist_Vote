c43b0f28d1e851c04bef902a049313de1f76a2bc
Brian Ruff <brff19@icloud.com>
2018-07-02 18:55:49 -0400

    sort by name functionality added in addition to sort by votes, error message for values below 0 added in for a visual representation to the user with a small box below the artist card affected.

--- components/Artist.js
+++ components/Artist.js
@@ -1,25 +1,82 @@
-import React from 'react';
+import React from "react";
 
-export default function Artist(props){
-    const isDead = props.death.isDead ? 'Death: ' + props.death.date : null
-    
-    function handleUpVote(){
-            props.upVote(props.id);
-    }
+export default function Artist(props) {
+  const isDead = props.death.isDead ? "Death: " + props.death.date : null;
 
-    function handleDownVote(){
-         props.downVote(props.id);
-    }
-    
-    return (
-        <div style={{margin: '1rem', textAlign: 'center'}}>
-            <h1>{props.name}</h1>
-            <img src={props.image} alt={props.image+props.name} height='250px' width="250px"/>
-            <p>DOB: {props.dob}</p>
-            <p>{isDead}</p>
-            <h3>Votes: {props.votes}</h3>
-            <button onClick={handleUpVote} style={{width: '100%', padding: '0.5rem'}}>Vote</button>
-            <button onClick={handleDownVote} style={{width: '100%', padding: '0.5rem'}}>Down Vote</button>
-        </div>
-    )
+  const errorMessage = (
+    <div
+      style={{
+        background: "#557182",
+        width: "100%",
+        color: "#FFF2E1",
+        height: "100px",
+        display: "flex",
+        justifyContent: "center",
+        alignContent: "center",
+        marginTop: "0.25rem"
+      }}
+    >
+      <p
+        style={{
+          alignSelf: "center"
+        }}
+      >
+        You can't vote an artist below zero!
+      </p>
+    </div>
+  );
+
+  function handleUpVote() {
+    props.handleUpVote(props.id);
+  }
+
+  function handleDownVote() {
+    props.handleDownVote(props.id);
+  }
+
+  return (
+    <div style={{ margin: "1rem", textAlign: "center" }}>
+      <h1>{props.name}</h1>
+      <img
+        src={props.image}
+        alt={props.image + props.name}
+        height="250px"
+        width="250px"
+      />
+      <p>DOB: {props.dob}</p>
+      <p>{isDead}</p>
+      <h3 style={{ color: "#333", textDecoration: "underline" }}>
+        Vote Count: {props.votes}
+      </h3>
+      <button
+        onClick={handleUpVote}
+        style={{
+          width: "100%",
+          padding: "0.5rem",
+          background: "#368A6C",
+          margin: "0.25rem 0",
+          borderRadius: "5px",
+          border: "transparent"
+        }}
+      >
+        Vote
+      </button>
+      <button
+        onClick={handleDownVote}
+        style={{
+          width: "100%",
+          padding: "0.5rem",
+          background: "rgb(188, 73, 102)",
+          margin: "0.25rem 0",
+          borderRadius: "5px",
+          border: "transparent"
+        }}
+      >
+        Down Vote
+      </button>
+      {
+        props.votes < 0 ? errorMessage : null
+      }
+    </div>
+  );
 }
--- components/ArtistList.js
+++ components/ArtistList.js
@@ -1,47 +1,74 @@
-import React from 'react';
-import Artist from './Artist';
+import React from "react";
+import Artist from "./Artist";
 
 export default class ArtistList extends React.Component {
-    state = {
-        artists: this.props.artists,
-    }
+  state = {
+    artists: this.props.artists
+  };
 
-    handleUpVote = (id) => {
-        const nextArtists = this.state.artists.map(artist => {
-            if(id === artist.id){
-                return Object.assign({}, artist, {
-                    votes: artist.votes + 1,
-                })
-            } else {
-                return artist
-            }
+  handleUpVote = id => {
+    const nextArtists = this.state.artists.map(artist => {
+      if (id === artist.id) {
+        return Object.assign({}, artist, {
+          votes: artist.votes + 1
         })
-        this.setState({artists: nextArtists})
-    }
+      } else {
+        return artist;
+      }
+    });
+    this.setState({ artists: nextArtists });
+  };
 
-    handleDownVote = (id) => {
-        const nextArtists = this.state.artists.map(artist => {
-            if(id === artist.id){
-                return Object.assign({}, artist, {
-                    votes: artist.votes - 1
-                })
-            } else {
-                return artist
-            }
-        })
-        this.setState({artists: nextArtists})
-    }
-    
-    render(){
-    const ArtistComponent = this.state.artists.sort((a,b) => b.votes - a.votes).map(({name, dob, death, image,votes, id}) => <Artist key={name} name={name} dob={dob} death={death} image={image} votes={votes} id={id} upVote={this.handleUpVote} downVote={this.handleDownVote} /> )
-        return (
-            <div style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignContent: 'center',
-            }}>
-               {ArtistComponent}
-            </div>
-        )
-    }
+  handleDownVote = id => {
+    const nextArtists = this.state.artists.map(artist => {
+      if (id === artist.id && artist.votes >= 0) {
+        return Object.assign({}, artist, {
+          votes: artist.votes - 1
+        });
+      } else if (artist.votes < 0) {
+        return Object.assign({}, artist, {
+          votes: artist.votes + 1
+        });
+      } else {
+        return artist;
+      }
+    });
+    this.setState({ artists: nextArtists });
+  };
+
+  render() {
+    const ArtistComponent = this.state.artists.sort((a,b) => b.name < a.name)
+      .sort((a, b) => (b.votes - a.votes))
+      .map(({ name, dob, death, image, votes, id }) => (
+        <Artist
+          key={name}
+          name={name}
+          dob={dob}
+          death={death}
+          image={image}
+          votes={votes}
+          id={id}
+          handleUpVote={this.handleUpVote}
+          handleDownVote={this.handleDownVote}
+        />
+      ));
+    return (
+      <div>
+        <h1 style={{ textAlign: "center", color: "#333", fontSize: "54px" }}>
+          Rockstars
+        </h1>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignContent: "center",
+            flexDirection: "row",
+            flex: "1"
+          }}
+        >
+          {ArtistComponent}
+        </div>
+      </div>
+    );
+  }
 }
